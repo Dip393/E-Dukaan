09060308523553c90076b94e02de06f8ef350d71
Dip393 <dbhattacharyya393@gmail.com>
2025-02-06 19:50:35 +0530

    Initial commit

---
+++ frontend/src/context/ShopContext.jsx
@@ -0,0 +1,61 @@
+import { createContext, useEffect, useState } from "react";
+import { products } from "../assets/assets";
+import { toast } from "react-toastify";
+
+export const ShopContext=createContext();
+
+const ShopContextProvider=(props)=>{
+    const currency='₹';
+    const delivery_fee=40;
+
+    const [search,setSearch]=useState('');
+    const [showSearch,setShowSearch]=useState(false);
+    const [cartItems,setCartItems]=useState({})
+
+    const addtoCart =async(itemId,size)=>{
+        if(!size){
+            toast.error('Select Product Size');
+            return;
+        }
+        let cartData=structuredClone(cartItems);
+        if(cartData[itemId]){
+            if(cartData[itemId][size]){
+                cartData[itemId][size]+=1;
+            }
+            else{
+                cartData[itemId][size]=1;
+            }
+        }
+        else{
+            cartData[itemId]={};
+            cartData[itemId][size]=1;
+        }
+        setCartItems(cartData);
+    }
+
+    const getCartCount=()=>{
+        let totalCount=0;
+        for (const items in cartItems){
+            for(const item in cartItems[items]){
+                try {
+                    if(cartItems[items][item]>0){}
+                } catch (error) {
+                    
+                }
+            }
+        }
+    }
+
+    const value={
+        products,currency,delivery_fee,
+        search,setSearch,showSearch,setShowSearch,
+        cartItems,setCartItems,addtoCart
+    }
+    return (
+        <ShopContext.Provider value={value}>
+            {props.children}
+        </ShopContext.Provider>
+    )
+}
+
+export default ShopContextProvider;
